a6125e5f3bc3387e251e0938c633be2527c13d5e
VictorKmin <kminvictor@gmail.com>
2019-06-08 13:21:36 +0300

    Add CLI

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const swaggerize = require('swaggerize-express');
-const path = require('path');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./config/swagger.json');
 
 require('dotenv').config();
 const dataBase = require('./dataBase').getInstance();
 dataBase.setModels();
+
+const swaggerize = require('swaggerize-express');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
+const {importOrders, importUsers} = require('./helpers/import');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -17,14 +21,23 @@ const program = require('commander');
 
 program
     .version('0.1.0')
-    .option('-p, --peppers', 'Add peppers')
+    .option('-u, --users', 'Add users to DB')
+    .option('-o, --orders', 'Add orders ti db')
     .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbqSauce) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+if (program.users) {
+    console.log('  ADD USERS  ....');
+    const [,,fileName] = process.argv;
+    console.log(fileName);
+    importUsers(fileName)
+}
+
+if (program.orders) {
+    console.log('  ADD ORDERS  ....');
+    const [,,fileName] = process.argv;
+    console.log(fileName);
+    importOrders(fileName)
+}
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
