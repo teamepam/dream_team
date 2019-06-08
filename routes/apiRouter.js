32e7dbda63f062676430aa77ed02f85685f78eed
Rutini <vl.patsuta@gmail.com>
2019-06-08 12:04:28 +0300

    v 1.9

--- routes/apiRouter.js
+++
@@ -1,9 +0,0 @@
-const ApiRouter = require('express').Router();
-
-const userRouter = require('./orders');
-
-// ApiRouter.use('/user', userRouter);
-// ApiRouter.use('/order', orderRouter);
-
-
-module.exports = ApiRouter;
